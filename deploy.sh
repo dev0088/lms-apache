ff4777b2c7158b149c29979813ad2d39c0905bd0
dev0088 <ninjadev999@gmail.com>
2020-03-31 03:33:53 +0000

    Added descriptions for install steps.

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-echo "Restarting apache2 service..."
-echo $1 | sudo systemctl reload apache2.service
+echo ""
+echo "1. Restarting apache2 service..."
+echo -e $1 | sudo systemctl reload apache2.service
 
-echo "Selecting node version manager..."
+echo ""
+echo "2. Selecting node version manager..."
 source /home/dev/.nvm/nvm.sh
 nvm use 18.15.0
 
-echo "Restarting websocket server..."
+echo ""
+echo "3. Restarting websocket server..."
 cd ./ws/server
 pm2 stop all
 npm install
 pm2 start server.js
 
+echo ""
 echo "Done!"
