e000a5cbd9b0f3401c98abea9ca4ea055ae70cef
danielmeja <daniel.m.v.e.89@gmail.com>
2020-03-30 20:25:59 -0700

    Added comments and Removed debug logs.

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+echo "Restarting apache2 service..."
 echo $1 | sudo systemctl reload apache2.service
 
+echo "Selecting node version manager..."
 source /home/dev/.nvm/nvm.sh
 nvm use 18.15.0
+
+echo "Restarting websocket server..."
 cd ./ws/server
 pm2 stop all
 npm install
 pm2 start server.js
 
+echo "Done!"
