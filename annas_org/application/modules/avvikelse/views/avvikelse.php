a777fc4cc1a879bb7bb301b6aeff4367f7d4823c
dev0088 <ninjadev999@gmail.com>
2020-04-16 10:24:15 +0000

    added permissions.

---
+++ annas_org/application/modules/avvikelse/views/avvikelse.php
@@ -0,0 +1,151 @@
+<style type="text/css">
+  div.box-content-input {
+    margin: 0 0 10px;
+  }
+  .input_option {
+    font-weight: 600;
+  }
+</style>
+<div class="content-wrapper">
+<!-- Content Header (Page header) -->
+<!-- Main content -->
+  <section class="content">
+  <!-- 
+  <?php if($this->session->flashdata("messagePr")){?>
+    <div class="alert alert-info">      
+      <?php echo $this->session->flashdata("messagePr")?>
+    </div>
+  <?php } ?> -->
+
+    <div class="row">
+      <div class="col-xs-12">
+        <div class="box box-success">
+        <?php if(!empty($user_data['id'])) { ?>
+          <form action="<?php echo base_url().'avvikelse/submit/'.$user_data['id']; ?>" method="post">
+        <?php } else { ?>
+        <form action="<?php echo base_url().'avvikelse/submit/'; ?>" method="post">
+        <?php } ?>
+            <div class="box-header with-border">
+              <h3 class="box-title" style="color: #00c0ef!important;font-weight:600;">Avvikelserapportering SoL</h3>
+              <div class="box-tools">
+                <button type="submit" class="btn-sm  btn btn-success modalButtonUser"><i class="glyphicon glyphicon-plus"></i> Submit</button>
+
+                <!-- 
+                <?php if(CheckPermission("users", "own_create")){ ?>
+                <button type="button" class="btn-sm  btn btn-success modalButtonUser" data-toggle="modal"><i class="glyphicon glyphicon-plus"></i> Add User</button>
+                <?php } if(setting_all('email_invitation') == 1){  ?>
+                <button type="button" class="btn-sm  btn btn-success InviteUser" data-toggle="modal"><i class="glyphicon glyphicon-plus"></i> Invite People</button>
+                <?php } ?> -->
+              </div>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+              <div class="col-xs-12 box-content-input">
+                <p class="input_option">Kundnamn</p><input type="text" name="customer_name" size="30" value="<?php echo (!empty($user_data['customer_name'])?$user_data['customer_name']:'');?>">
+              </div>
+              <div class="col-xs-12 box-content-input">
+                <p class="input_option">Zon</p><input type="text" name="zone" size="30" value="<?php echo (!empty($user_data['zone'])?$user_data['zone']:'');?>">
+              </div>
+              <div class="col-xs-12 box-content-input">
+                <p class="input_option">Personal</p><input type="text" id="usr_name" name="usr_name" size="30" value="<?php echo (!empty($user_data['usr_name'])?$user_data['usr_name']:$usr_data['name']);?>">
+              </div>
+              <div class="col-xs-12 box-content-input">
+                <p class="input_option">Alternativt flera brukare berörda av händelsen</p><input type="text" name="several_users" size="30" value="<?php echo (!empty($user_data['several_users'])?$user_data['several_users']:'');?>">
+              </div>
+              <div class="col-xs-12 box-content-input">
+                <p class="input_option">Händelsedatum</p>
+                <input type="text" id="datepicker" name="date" size="30" value="<?php echo (!empty($user_data['date'])?$user_data['date']:'');?>">
+                <p style="color:gray;">* Cliquez ou appuyez ici pour entrer une date.</p>
+              </div>
+              <div class="col-xs-12 box-content-input">
+                <p class="input_option">Händelsetid</p><input type="time" name="time" size="15" value="<?php echo (!empty($user_data['time'])?$user_data['time']:'');?>">
+              </div>
+              <div class="col-xs-12 box-content-input">
+                <p class="input_option">Händelseplats*</p><input type="text" name="location" size="30" value="<?php echo (!empty($user_data['location'])?$user_data['location']:'');?>">
+                <p style="color:gray;">* Ange var – ordinärt boende, särskilt boende, utomhus eller annan plats</p>
+              </div>
+              <div class="col-xs-12 box-content-input">
+                <p class="input_option">• Händelsebeskrivning (beskriv detaljerat vad sominträffat)</p><textarea name="course" rows="8" style="width:100%"><?php echo (!empty($user_data['course'])?$user_data['course']:'');?></textarea>
+              </div>
+              <div class="col-xs-12 box-content-input">
+                <p class="input_option">• Typ av händelse</p>
+                <input type="checkbox" class="event_checkbox" id="event1" name="event_checkbox" value="Brist i bemötande">
+                <label for="event1"> Brist i bemötande</label><br>
+                <input type="checkbox" class="event_checkbox" id="event2" name="event_checkbox" value="Bristande tillgänglighet">
+                <label for="event2"> Bristande tillgänglighet</label><br>
+                <input type="checkbox" class="event_checkbox" id="event3" name="event_checkbox" value="Brist i/utebliven handläggning eller dokumentation (HSL, SOL, LSS)">
+                <label for="event3"> Brist i/utebliven handläggning eller dokumentation (HSL, SOL, LSS)</label><br>
+                <input type="checkbox" class="event_checkbox" id="event4" name="event_checkbox" value="Brist i/utebliven insats">
+                <label for="event4"> Brist i/utebliven insats</label><br>
+                <input type="checkbox" class="event_checkbox" id="event5" name="event_checkbox" value="Brist i/utebliven vårdåtgärd">
+                <label for="event5"> Brist i/utebliven vårdåtgärd</label><br>
+                <input type="checkbox" class="event_checkbox" id="event6" name="event_checkbox" value="Brist gällande samtycke från brukare">
+                <label for="event6"> Brist gällande samtycke från brukare</label><br>
+                <input type="checkbox" class="event_checkbox" id="event7" name="event_checkbox" value="Brist i information, kommunikation, samverken">
+                <label for="event7"> Brist i information, kommunikation, samverken</label><br>
+                <input type="checkbox" class="event_checkbox" id="event8" name="event_checkbox" value="Läkemedelshändelse">
+                <label for="event8"> Läkemedelshändelse</label><br>
+                <input type="checkbox" class="event_checkbox" id="event9" name="event_checkbox" value="Fall">
+                <label for="event9"> Fall</label><br>
+                <input type="checkbox" class="event_checkbox" id="event10" name="event_checkbox" value="Vård- och omsorgsskada">
+                <label for="event10"> Vård- och omsorgsskada</label><br>
+                <input type="checkbox" class="event_checkbox" id="event11" name="event_checkbox" value="Händelse med produkter och/eller teknik">
+                <label for="event11"> Händelse med produkter och/eller teknik</label><br>
+                <input type="checkbox" class="event_checkbox" id="event12" name="event_checkbox" value="Bruten sekretess">
+                <label for="event12"> Bruten sekretess</label><br>
+                <input type="checkbox" class="event_checkbox" id="event13" name="event_checkbox" value="Fysiskt, psykiskt, sexuellt eller ekonomiskt övergrepp">
+                <label for="event13"> Fysiskt, psykiskt, sexuellt eller ekonomiskt övergrepp</label><br>
+                <input type="checkbox" class="event_checkbox" id="event14" name="event_checkbox" value="Misstanke om självmord">
+                <label for="event14"> Misstanke om självmord</label><br>
+                <input type="checkbox" class="event_checkbox_annat" id="event15" name="event_checkbox_annat" value="Annat">
+                <label for="event15"> Annat</label><br>
+                <textarea name="type_event" id="event_textarea" rows="2" style="width:100%"><?php echo (!empty($user_data['type_event'])?$user_data['type_event']:'');?></textarea>
+
+              </div>              
+              <div class="col-xs-12 box-content-input">
+                <p class="input_option">• Ev. direkt utförd handling för att undvika mer skada</p><textarea name="suggestion" rows="5" style="width:100%"><?php echo (!empty($user_data['suggestion'])?$user_data['suggestion']:'');?></textarea>
+              </div>
+              <div class="col-xs-12 box-content-input">
+                <p class="input_option" style="background: lightblue;">Verksamhetschefs åtgärder</p><textarea name="admin_remark" rows="5" style="width:100%"><?php echo (!empty($user_data['admin_remark'])?$user_data['admin_remark']:'');?></textarea>
+              </div>
+              
+              <!-- <div class="col-xs-12 box-content-input">
+                <p class="input_option">Händelseförloppet:</p><textarea name="course" rows="10" style="width:100%"><?php echo (!empty($user_data['course'])?$user_data['course']:'');?></textarea>
+              </div> -->
+              <!-- <div class="col-xs-12 box-content-input">
+                <p class="input_option">Eventuella åtgärdsförslag:</p><textarea name="suggestion" rows="3" style="width:100%"><?php echo (!empty($user_data['suggestion'])?$user_data['suggestion']:'');?></textarea>
+              </div> -->
+
+            </div>
+            <!-- /.box-body -->
+          </form>
+        </div>
+        <!-- /.box -->
+      </div>
+      <!-- /.col -->
+    </div>
+    <!-- /.row -->
+  </section>
+  <!-- /.content -->
+</div> 
+<script type="text/javascript">
+  $(document).ready(function() {  
+    $( "#datepicker" ).datepicker({ dateFormat: 'dd-mm-yy' });
+    $("input.event_checkbox").click(function () {
+      var val = [];
+      $("#event15").prop("checked", false);
+      $('#event_textarea').val('');
+      $("input.event_checkbox:checked").each(function(i){
+        val[i] = ' ' + $(this).val();
+        $('#event_textarea').val(val);
+      });
+    });
+    $("#event15").click(function () {
+      $('#event_textarea').val('');
+      // $("input.event_checkbox").prop("checked", false);
+      // $("input.event_checkbox").attr('checked', false);
+      $(".event_checkbox").prop("checked", false);
+      // $(".event_checkbox:checkbox").removeAttr("checked");
+    });
+  });
+</script>
---
+++ annas_org/application/modules/socialjournal/controllers/socialjournal.php
@@ -0,0 +1,130 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed ');
+class Socialjournal extends CI_Controller {
+
+    function __construct() {
+        parent::__construct(); 
+		$this->load->model('Socialjournal_model');
+		$this->user_id = isset($this->session->get_userdata()['user_details'][0]->id)?$this->session->get_userdata()['user_details'][0]->users_id:'1';
+    }
+
+    public function index() {
+    	 is_login();
+        $this->load->view('include/header');
+        $this->load->view('socialjournal');
+        $this->load->view('include/footer');
+    }
+
+    public function submit($id='') {
+        is_login();
+        if($this->input->post()){
+            $customer_data['users_id'] = $this->session->userdata('user_details')[0]->users_id;
+            $customer_data['date'] = $this->input->post('date');
+            $customer_data['customer_name'] = $this->input->post('customer_name');
+            $customer_data['usr_name'] = $this->input->post('usr_name');
+            $customer_data['several_users'] = $this->input->post('several_users');
+            $customer_data['course'] = $this->input->post('course');
+            $customer_data['time'] = $this->input->post('time');
+            $customer_data['location'] = $this->input->post('location');
+            $customer_data['zone'] = $this->input->post('zone');
+            $customer_data['type_event'] = $this->input->post('type_event');
+            if( !empty($id) ){
+              $this->Socialjournal_model->updateRow('socialjournal', 'id', $id, $customer_data);
+            } else {
+              $this->Socialjournal_model->insertRow('socialjournal',$customer_data);
+            }
+            $result = $this->Socialjournal_model->get_data_by('socialjournal','', 'id', 'desc');
+            $customer_id = $result[0]->id;
+            $forum_data['date'] = $customer_data['date'];
+            $forum_data['customer_name'] = $this->input->post('customer_name');
+            $forum_data['zone'] = $customer_data['zone'];
+            $forum_data['type_event'] = $customer_data['type_event'];
+            $forum_data['form'] = 'socialjournal';
+            $forum_data['html_file'] = "data/socialjournal_".$customer_data['customer_name']."_".$customer_data['date']."_".$customer_data['users_id'].".html";
+            $forum_data['form_id'] = $customer_id;
+            $forum_data['users_id'] = $customer_data['users_id'];
+            $forum_data['usr_name'] = $customer_data['usr_name'];
+            if( !empty($id) ) {
+              $this->Socialjournal_model->updateRow_M('forum', 'form_id', $id, 'form', 'socialjournal', $forum_data);
+            } else {
+              $this->Socialjournal_model->insertRow('forum',$forum_data);
+            }
+            ?>
+
+            <?php
+            ob_start();
+            ?>
+            <html>
+            <meta charset="UTF-8">
+            <body>
+              <div class="box-body">
+                <div class="col-xs-12" style="text-align:center; margin-bottom: 10px;">
+                  <h1 style="color: #00c0ef!important;">SOCIAL JOURNAL FÖR GENOMFÖRANDE</h1>
+                  <h1 style="color: #00c0ef!important;">(ENLIGT SOL OCH LSS)</h1>
+                  <h4 style="color: #00c0ef!important;">Annas vård och hemtjänstteam AB</h4>
+                </div>
+                <div class="col-xs-12" style="margin-bottom: 10px;">
+                  <h4 style="font-weight: 600;">Kundnamn: <span style="font-weight: normal;"><?php echo $customer_data['customer_name'];?></span></h4>
+                  <h4 style="font-weight: 600;">Zon: <span style="font-weight: normal;"><?php echo $customer_data['zone'];?></span></h4>
+                </div>
+                <div class="col-xs-12" style="margin-bottom: 10px;">
+                  <h4 style="font-weight: 600;">Personal: <span style="font-weight: normal;"><?php echo $customer_data['usr_name'];?></span></h4>
+                </div>
+                <div class="col-xs-12" style="margin-bottom: 10px;">
+                  <h4 style="font-weight: 600;">Alternativt flera brukare berörda av händelsen: <span style="font-weight: normal;"><?php echo $customer_data['several_users'];?></span></h4>
+                </div>
+                <div class="col-xs-12" style="margin-bottom: 10px;">
+                  <h4 style="font-weight: 600;">Händelsedatum & tid: <span style="font-weight: normal;"><?php echo $customer_data['date'];?> <?php echo $customer_data['time'];?></span></h4>
+                </div>
+                <div class="col-xs-12" style="margin-bottom: 10px;">
+                  <h4 style="font-weight: 600;">Händelseplats*: <span style="font-weight: normal;"><?php echo $customer_data['location'];?></span></h4>
+                </div>
+                <div class="col-xs-12" style="margin-bottom: 10px;">
+                  <h4 style="font-weight: 600;">• Typ av händelse</h4>
+                  <h4 style="font-weight: normal;"><?php echo $customer_data['type_event'];?></h4>
+                </div>
+                <div class="col-xs-12" style="min-height: 150px; margin-bottom: 10px;">
+                  <h4 style="font-weight: 600;">• Händelsebeskrivning (beskriv detaljerat vad sominträffat)</h4>
+                  <h4 style="font-weight: normal;"><?php echo $customer_data['course'];?></h4>
+                </div>
+              </div>
+            </body>
+            </html>
+            <?php
+            file_put_contents("data/socialjournal_".$customer_data['customer_name']."_".$customer_data['date']."_".$customer_data['users_id'].".html", ob_get_contents());
+            ?>
+            <?php
+            header("Location: ". base_url()."user/forum");
+        } else {
+            $this->load->view('include/header'); 
+            $this->load->view('socialjournal');
+            $this->load->view('include/footer');
+        }
+    }
+
+    public function form($id='') {
+        is_login();
+        $this->load->view('include/header');        
+        if(!empty($id)){
+          $result_data = $this->Socialjournal_model->get_data_by('socialjournal',$id, 'id');
+          $customer_data['id'] = $id;
+          $customer_data['date'] = $result_data[0]->date;
+          $customer_data['time'] = $result_data[0]->time;
+          $customer_data['location'] = $result_data[0]->location;
+          $customer_data['customer_name'] = $result_data[0]->customer_name;
+          $customer_data['zone'] = $result_data[0]->zone;
+          $customer_data['usr_name'] = $result_data[0]->usr_name;
+          $customer_data['several_users'] = $result_data[0]->several_users;
+          $customer_data['course'] = $result_data[0]->course;
+          $customer_data['type_event'] = $result_data[0]->type_event;
+          $data['user_data'] = $customer_data;
+          $this->load->view('socialjournal', $data);
+        } else {
+          $user['name'] = $this->session->userdata ('user_details')[0]->name;
+          $data1['usr_data'] = $user;
+          $this->load->view('socialjournal', $data1);
+        }
+        $this->load->view('include/footer');
+    }
+
+}
